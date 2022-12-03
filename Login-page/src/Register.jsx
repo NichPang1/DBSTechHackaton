87048736e41ff88c50c50855dd1ddbf78b8d3d22
Roy Teoh <106427998+RoyTeoh@users.noreply.github.com>
2022-12-03 16:11:46 +0800

    Create Register.jsx

---
+++ Login-page/src/Register.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+export const Register = (props) => {
+    const[email, setEmail] = useState("");
+    const[pass, setPass] = useState("");
+    const[name, SetName] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //if dont page get reloaded and lose state
+        console.log(email);
+
+    }
+    return (
+        <div className = 'auth-from-container'>
+            <form className = "register-form" onSubmit ={handleSubmit}>
+                <label htmlFor ="name">Full name</label>
+                <input value = {name} name="name" id = "name" placeholder = "fullname"/>
+                <label htmlFor ="email">email</label>
+                <input value = {email} onChange ={(e) => setEmail(e.target.value)}type = "email" placeholder = "e.g youremail@gmail.com" id = "email" name = "email"/>
+                <label htmlFor ="password">password</label>
+                <input value = {pass} type = "password" placeholder = "******" id = "password" name = "password"/>
+                <button type = "submit">Register</button>
+            </form>
+            <a onClick ={()=> props.onFormSwitch("login")}>Already have an account? Login here</a>
+        </div>
+    )
+}

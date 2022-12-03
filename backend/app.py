5eac2458f93a91fc776e19143b94bec68b558c53
sm <wongsm7@hotmail.com>
2022-12-03 13:05:15 +0800

    Update feature 6 to check if email or address is the same

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,8 @@ app.config['MYSQL_HOST'] = os.getenv("DB_HOST")
 app.config['MYSQL_USER'] = os.getenv("DB_USERNAME")
 app.config['MYSQL_PASSWORD'] = os.getenv("DB_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("DB_NAME")
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor' # return items as array of objects
+# return items as array of objects
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # secret for jwt
 app.config['SECRET_KEY'] = '004f2af45d3a4e161a7dd2d17fdae47f'
@@ -29,6 +30,7 @@ load_dotenv()
 # setup mysql
 mysql = MySQL(app)
 
+
 @app.route("/hello")
 def create_user():
     return 'Hello World!'
@@ -40,9 +42,22 @@ def update_user():
     try:
         params = request.args
         cursor = mysql.connection.cursor()
+        resultValue = cursor.execute('select * from user where userId = {}'.format(params.get('userId')))
+        if resultValue == 0:
+            return make_response('User not found',  403)
+        userDetails = cursor.fetchall()
+        if userDetails[0]['Email'] ==  params.get('email'):
+            return make_response('Email is the same!',  403)
+        if userDetails[0]['Address'] ==  params.get('address'):
+            return make_response('Address is the same!',  403)
+        cursor = mysql.connection.cursor()
         cursor.execute("UPDATE user SET `email` = '{}', `address` = '{}' WHERE `userid` = '{}'".format(
             params.get('email'), params.get('address'), params.get('userId')))
         mysql.connection.commit()
-        return 'User updated successfully'
+        # resultValue = cursor.execute('select * from user')
+        # if resultValue > 0:
+        #     userDetails = cursor.fetchall()
+        #     return jsonify(userDetails)
+        return make_response('User updated successfully', 200)
     except Exception:
         return 'Failed to update user'
